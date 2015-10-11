562f60f3f059e230c4a3ff0e4ec26d01b8d8e424
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-10-12 01:03:37 +0900

    circleci has old openssl, use docker image for test

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -13,8 +13,20 @@ describe "nginx_omniauth_helper integration" do
     skip "nginx required" unless system('nginx', '-V', err: IO::NULL, out: IO::NULL)
 
     nginx_pid = spawn('nginx', '-c', File.join(example_dir, 'nginx.conf'))
-    adapter_pid = spawn({'NGX_OMNIAUTH_HOST' => 'http://ngx-auth.127.0.0.1.xip.io:18080'}, 'rackup', '-p', '18081', '-o', '127.0.0.1', File.join(__dir__, '..', 'config.ru'), out: IO::NULL, err: IO::NULL)
     backend_pid = spawn('ruby', File.join(example_dir, 'test_backend.rb'), '-p', '18082', '-o', '127.0.0.1', out: IO::NULL, err: IO::NULL)
+
+    if ENV['ADAPTER_DOCKER']
+      adapter_pid = spawn(
+        'docker', 'run',
+        '--env', 'RACK_ENV=test',
+        '--env', 'NGX_OMNIAUTH_HOST=http://ngx-auth.127.0.0.1.xip.io:18080',
+        '-p', '18080:8080',
+        ENV['ADAPTER_DOCKER'],
+        out: IO::NULL, err: $stderr
+      )
+    else
+      adapter_pid = spawn({'NGX_OMNIAUTH_HOST' => 'http://ngx-auth.127.0.0.1.xip.io:18080'}, 'rackup', '-p', '18081', '-o', '127.0.0.1', File.join(__dir__, '..', 'config.ru'), out: IO::NULL, err: $stderr)
+    end
 
     10.times do
       begin
